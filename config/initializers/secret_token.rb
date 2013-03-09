bbda10bbc41799b977124a89b8ed578539596858
tnantoka <bornneet@livedoor.com>
2013-03-10 00:41:33 +0900

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Konnyaku::Application.config.secret_key_base = '9ba3fbe395bf1bdecda1d41871bedf4901ad1c0e5053332ea6292c84017d2b5bd18b4e7045360a00fc45a889298d91a0a094b69b0ee339c5b8678e37b77019fd'
