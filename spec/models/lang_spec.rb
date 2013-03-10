576a782438b22a24f90dcd6dd2b2d0aa395fc753
tnantoka <bornneet@livedoor.com>
2013-03-10 23:17:11 +0900

    Add validation for lang

--- spec/models/lang_spec.rb
+++ spec/models/lang_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe Lang do
+
+  describe '.create' do
+    context 'with valid code' do
+      after(:each) do
+        I18n::available_locales = [:en, :ja]
+      end
+      it 'creates new lang' do
+        I18n::available_locales = [:ch]
+        expect { Lang.create!(id: 3, code: 'ch') }.to_not raise_error
+      end
+    end
+    context 'with invalid code' do
+      it 'raises invalid exception' do
+        expect { Lang.create!(id: 3, code: 'aa') }.to raise_error
+      end
+    end
+    context 'with duplicated code' do
+      it 'raises invalid exception' do
+        expect { Lang.create!(id: 3, code: 'ja') }.to raise_error
+      end
+    end
+  end
+
 end
