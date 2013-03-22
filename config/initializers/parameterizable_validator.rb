2f74c35845aa8d44fc4c39bb9e3f79cb486e4d4f
tnantoka <bornneet@livedoor.com>
2013-03-22 17:29:11 +0900

    Add 'new' to blacklist of slug

--- config/initializers/parameterizable_validator.rb
+++ config/initializers/parameterizable_validator.rb
@@ -1,5 +1,5 @@
 class ParameterizableValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attr, :invalid) if value.present? && value != value.parameterize 
+    record.errors.add(attribute, :invalid) if value.present? && (value != value.parameterize || value.parameterize == 'new')
   end
 end
